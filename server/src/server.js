8d86194893f2876f36095ea5eba786fe57a6de9e
Barry-White17 <whitwb2@gmail.com>
2026-09-25 08:10:09 +0100

    Update database connection to use environment variable

--- server/src/server.js
+++ server/src/server.js
@@ -4,9 +4,9 @@ import mongoose from 'mongoose'
 
 // Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri)
+mongoose.connect(process.env.DATABASE_URL)
 mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+  throw new Error(`unable to connect to database: ${process.env.DATABASE_URL}`)
 })
 
 app.listen(config.port, (err) => {
